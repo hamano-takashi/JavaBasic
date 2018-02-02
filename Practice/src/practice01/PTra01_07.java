978cdd671798bc0cfda72ca6be5d3c104bf43554
takashi <hamerkean0803@yahoo.co.jp>
2018-02-02 21:32:50 +0900

    演習問題5の条件分岐まで終わりました。

--- Practice/src/practice01/PTra01_07.java
+++ Practice/src/practice01/PTra01_07.java
@@ -20,7 +20,17 @@ public class PTra01_07 {
 		double d;
 
 		// ★ それぞれの型変数に、値を代入してください
-		// ※ 値は、好きな数字を入力してください
+		// ※ 値は、好きな数字を入力してくだ
+		bo = true;
+		by = 5;
+		s = 10;
+		c = 'あ';
+		i = 20;
+		l = 50;
+		f = 2.89f;
+		d = 3.93;
+		
+		
 
 
 
@@ -31,6 +41,15 @@ public class PTra01_07 {
 
 
 		// ★ それぞれの変数の中身を出力してください
+		System.out.println(bo);
+		System.out.println(5);
+		System.out.println(10);
+		System.out.println('あ');
+		System.out.println(20);
+		System.out.println(50);
+		System.out.println(2.89f);
+		System.out.println(3.93);
+
 
 
 
